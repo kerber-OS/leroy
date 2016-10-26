20433e40d609f8a46c2168daa6e199c5ea662a69
kerber-OS <voff.sergeev@gmail.com>
2016-10-26 10:29:44 +0400

    learning list C#

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,21 +10,27 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
+ 
+            Point p1 = new Point(1, 3, '*');
 
-            p1.Draw();
+            Point p2 = new Point(4, 5, '#');
 
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
+            Point p3 = new Point(5, 6, '7');
 
-            p2.Draw();
+            Point p4 = new Point(7, 8, '8');
+     
+            List<Point> pList = new List<Point>();
 
-            Console.ReadLine();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+
         }
 
         
