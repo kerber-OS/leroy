0ea34685da0e966c1ac041fc6b0cd29a571708bb
kerber-OS <voff.sergeev@gmail.com>
2016-10-26 10:54:50 +0400

    hsh

---
+++ Snake/Snake/HorizontalLine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HorizontalLine
+    {
+        List<Point> pList;
+
+        public HorizontalLine(int xLeft, int xRaight, int y, char sym)
+        {
+            pList = new List<Point>();
+            for (int x = xLeft; x <= xRaight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+
+        }
+    }
+}
