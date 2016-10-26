e771d789721f03b145cbefd4d93337e2868b1e81
kerber-OS <voff.sergeev@gmail.com>
2016-10-26 10:51:06 +0400

    haha

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,23 +10,22 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
 
-            p1.Draw();
+            Point p1 = new Point(1, 3, '*');
 
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
+            Point p2 = new Point(4, 5, '#');
 
-            p2.Draw();
+            Point p3 = new Point(5, 6, '7');
+
+            Point p4 = new Point(7, 8, '8');
+
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+
+            line.Draw();
 
             Console.ReadLine();
         }
 
-        
+
     }
 }
